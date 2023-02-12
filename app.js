ada889b8aa21c4ba6294aaf7b858ad5c2df70ac8
NahuelEF <nahuelfedele@gmail.com>
2023-02-12 00:20:02 -0300

    Fix: validate email

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 var email = document.getElementById("email");
-const error_message = document.querySelector("p");
+var error_message = document.getElementById("error");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,14 +10,22 @@ form.addEventListener("submit", (e) => {
 const validate = () => {
   var emailValue = email.value.trim();
   const values = /(gmail.com|hotmail.com|yahoo.com)/;
-  
-  if (!emailValue.match(values)) {
-    email.classList.add("input__email--error");
-    error_message.classList.add("error-message--block");
-    email.focus();
+
+  if (emailValue === "") {
+    setErrorMessage("Whoops! It looks like you forgot to add your email");
+  } else if (!emailValue.match(values)) {
+    setErrorMessage("Please provide a valid email address");
   } else {
     email.classList.remove("input__email--error");
     error_message.classList.remove("error-message--block");
     email.value = "";
+    console.log(emailValue);
   }
+};
+
+const setErrorMessage = (message) => {
+  error_message.innerHTML = `${message}`;
+  email.classList.add("input__email--error");
+  error_message.classList.add("error-message--block");
+  email.focus();
 };
